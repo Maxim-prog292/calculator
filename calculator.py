e7bde4ec3f966da1a47939ae7239e78f6bd6e61a
Maxim-prog292 <78023667+Maxim-prog292@users.noreply.github.com>
2021-02-17 11:38:06 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,54 @@
+import math
+
+print("                            CALCULATE")
+print("ОПЕРАЦИИ")
+print("Сложенте: +\nВычитание: -\nУмножение: *\nДеление: / \nВозведение в степень: **\nИзвлечение квадратного корня: √\n")
+
+print("Введите числа, над которыми хотите провести операцию -->")
+
+
+firstNum = int(input("Первое число: "))
+
+do = input("Операция: ")
+
+tooNum = int(input("Второе число: "))
+
+
+#сложение
+
+if do == "+":
+	sum = firstNum + tooNum
+	print("Ответ: " + str(float(sum)))
+	
+#вычитание
+
+elif do == "-":
+	if firstNum > tooNum:
+		razn = firstNum - tooNum
+		print(razn)
+	else:
+		razn = tooNum - firstNum
+		print("Ответ: " + str(float(razn)))
+
+#умножение
+
+elif do == "*":
+	umn = firstNum * tooNum
+	print("Ответ: " + str(float(umn)))
+	
+#деление
+
+elif do == "/":
+	div = firstNum / tooNum
+	print("Ответ: " + str(float(div)))
+	
+#возведение в степень
+
+elif do == "**":
+	step = firstNum ** tooNum
+	print("Ответ: " + str(float(step)))
+	
+#извлечение квадратного корня
+
+elif (firstNum == 0 and do =="√"):
+	print(math.sqrt(tooNum))
